0a6d388fef6c4541efeb38367f9fba68029d38af
Kevin Iturralde <keviniturralde3@gmial.com>
2020-11-12 16:34:09 -0500

    second

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 const getFirstArtist = () => {
     fetch('http://localhost:3000/api/v1/artists/10')
     .then(r => r.json())
-    .then( renderArtist )
+    .then( renderPainting )
+}
+const paintingImg = document.querySelector('#image')
+const paintingName = document.querySelector("#painting-name")
+const artistNames = document.querySelector(".artist-name")
+
+const renderPainting = paintingObj => {
+    paintingImg.src = paintingObj.paintings[1].painting_url
+    const name = paintingObj.paintings[1].name
+    paintingName.innerHTML = `${name}`
+    renderAtribute(paintingObj)
+    for(let id = 10; id === 13; id++ ){
+        fetch(`http://localhost:3000/api/v1/artists/${id}`)
+            .then(r => r.json())
+            .then(console.log)
+    }
+    
+    // renderArtists(paintingObj)
+
 }
 
-const renderArtist = artistObj => {
-    const paintingImg = document.querySelector('#image')
-    paintingImg.src = artistObj.paintings[1].painting_url
-        
-
+const renderArtists = artistObj => {
+    const li = document.createElement("li")
+    li.textContent = artistObj.name
+    artistNames.append(li)
 }
+
+const renderAtribute = attributeObj => {
+    const attribute = document.querySelector(".attribute")
+    const li = document.createElement("li")
+    li.textContent = attributeObj.paintings[1].painting_inspiration 
+    attribute.append(li)
+}
+
+
+
+
 getFirstArtist()
