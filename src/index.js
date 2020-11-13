b4d986d4a49a47473aab363dda70aa1819a1ecb1
Kevin Iturralde <keviniturralde3@gmial.com>
2020-11-13 14:43:42 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const getFirstArtist = () => {
 const paintingImg = document.querySelector('#image')
 const paintingName = document.querySelector("#painting-name")
 const artistNames = document.querySelector(".artist-name")
+const artistButton = document.querySelector(".button")
 
 const renderPainting = paintingObj => {
     paintingImg.src = paintingObj.paintings[1].painting_url
     const name = paintingObj.paintings[1].name
-    paintingName.innerHTML = `${name}`
+    paintingName.innerHTML = name
     renderAtribute(paintingObj)
+    renderArtists(paintingObj)
     for(let id = 10; id === 13; id++ ){
         fetch(`http://localhost:3000/api/v1/artists/${id}`)
             .then(r => r.json())
@@ -21,7 +23,6 @@ const renderPainting = paintingObj => {
     // renderArtists(paintingObj)
 
 }
-
 const renderArtists = artistObj => {
     const li = document.createElement("li")
     li.textContent = artistObj.name
@@ -35,6 +36,30 @@ const renderAtribute = attributeObj => {
     attribute.append(li)
 }
 
+const paintingForm = document.querySelector("#new-painting")
+    paintingForm.addEventListener("submit", event => {
+        event.preventDefault()
+fetch("http://localhost:3000/api/v1/paintings", {
+    method: "POST", 
+    headers: {
+  "Content-Type": "application/json"
+},
+    body: JSON.stringify ({
+    name: event.target.name.value,
+    painting_url: event.target.painting_url.value
+})
+})
+    .then(r => r.json())
+    .then(newPainting => {
+        const name = newPainting.name
+        const url = newPainting.painting_url
+        paintingName.textContent = name
+        paintingImg.src = url
+
+    })
+
+
+} )
 
 
 
